51e916191330984dff2ad19da57c9fd6fa104a0a
Frano <frano123suman@gmail.com>
2024-03-28 15:04:45 +0100

    pravljenje main func

--- sinus.py
+++ sinus.py
@@ -68,54 +68,104 @@ def switch_waves(option,amplitude = 1,time = 1,frequency = 1,sample_rate = 44100
    return options.get(option,"Default")
 
 
-amplitude = st.sidebar.number_input("Odaberi amplitudu [V]", 0.1, 230.0,1.)
-sample_rate = st.sidebar.slider("Odaberi SAMPLE RATE [Hz]", 1, 44100,44100)
-st.sidebar.info("Frekvencija se resetira promjenom sampling-a (Nyquist frequiency)")
-frequency = st.sidebar.number_input("Odaberi frekevenciju [Hz]", 1, int(sample_rate/2)) #nyquist frequency
-time = st.sidebar.number_input("Odaberi trajanje u sekundama [s]", 1, 60)
+#amplitude = st.sidebar.number_input("Odaberi amplitudu [V]", 0.1, 230.0,1.)
+#sample_rate = st.sidebar.slider("Odaberi SAMPLE RATE [Hz]", 1, 44100,44100)
+#st.sidebar.info("Frekvencija se resetira promjenom sampling-a (Nyquist frequiency)")
+#frequency = st.sidebar.number_input("Odaberi frekevenciju [Hz]", 1, int(sample_rate/2)) #nyquist frequency
+#time = st.sidebar.number_input("Odaberi trajanje u sekundama [s]", 1, 60)
 
-pick_wave_gen = st.sidebar.radio(
-   "Odaberi val koji generirati",
-   [
-      "Sin","Cos","Sawtooth","Tirangle","Square","White noise","Brown noise"
-   ]
-)
+#pick_wave_gen = st.sidebar.radio(
+ #  "Odaberi val koji generirati",
+  # [
+   #   "Sin","Cos","Sawtooth","Tirangle","Square","White noise","Brown noise"
+   #]
+#)
 #col1, col2 = st.columns(2)
 #st.sidebar.caption("***Gangsta***")
 #st.slider('Select a range of values',0, num_samples, (5,50))
 #st.sidebar.toggle("Napravi analizu signala")
 
-st.header("ANALIZA SINALA")
+#st.header("ANALIZA SINALA")
 
 
 
-y = switch_waves(pick_wave_gen,amplitude,time,frequency,sample_rate)
+#y = switch_waves(pick_wave_gen,amplitude,time,frequency,sample_rate)
 #st.sidebar.button("Generate Tone",on_click=generate_wave(signal))
-Umax = max(y[:N_uzoraka])
-Umin = min(y[:N_uzoraka])
-Udc = dc_component(y)
-Uef = efective(y)
-σ = sp.ndimage.standard_deviation(y[:N_uzoraka])
-γ = (σ/Udc) * 100
-Psr = Uef**2
-Psr_dBW = 20 * np.log10(Uef) 
-rounded_values = [round(x,16) for x in [Umax,Umin,Udc,Uef,σ,γ,Psr,Psr_dBW ]]
+#Umax = max(y[:N_uzoraka])
+#Umin = min(y[:N_uzoraka])
+#Upp = Umax - Umin
+#Udc = dc_component(y)
+#Uef = efective(y)
+#standard_deviation = sp.ndimage.standard_deviation(y[:N_uzoraka])
+#gamma = float('inf') if (standard_deviation/Udc) > 100000.0 else (standard_deviation/Udc) * 100
+#Psr = Uef**2
+#Psr_dBW = 20 * np.log10(Uef) 
+#rounded_values = [round(x,16) for x in [Umax,Umin,Upp,Udc,Uef,standard_deviation,gamma,Psr,Psr_dBW ]]
 
-table = pd.DataFrame( {
-                          "Values":rounded_values,
-                          #"":["V","V","V","V","V","%"]
-                          },
-                        index= ["Umax","Umin","Udc","Uef","σ","γ","Psr","Psr_dBW "])
+#table = pd.DataFrame( {
+ #                         "Values":rounded_values,
+  #                        #"":["V","V","V","V","V","%"]
+   #                       },
+    #                    index= ["Umax","Umin","Upp","Udc","Uef","σ","γ","Psr","Psr_dBW "])
 
-start,end = st.slider('Podesi slider za ublizavanje na val (skalirano je po sampling * vrijeme)',0, sample_rate*time,(0,sample_rate*time))    
+#start,end = st.slider('Podesi slider za ublizavanje na val (skalirano je po sampling * vrijeme)',0, sample_rate*time,(0,sample_rate*time))    
 
-on = st.checkbox("Napravi analizu signala")
+#on = st.checkbox("Napravi analizu signala [uzima se do prvih 1,2M uzoraka]")
 
-st.write(gen_plot(y[start:end],Umax,Umin,Udc,Uef,on))
+#st.write(gen_plot(y[start:end],Umax,Umin,Udc,Uef,on))
 
 # Ensure the values are in 16-bit range
-audio = np.int16(y * 32767)
+#audio = np.int16(y * 32767)
 # Write to a MP3 file
-sp.io.wavfile.write('wave.mp3', sample_rate, audio)
-st.audio('wave.mp3',)
-st.dataframe(table,width=300)
+#sp.io.wavfile.write('wave.mp3', sample_rate, audio)
+#st.audio('wave.mp3')
+#st.dataframe(table,width=300)
+
+
+if __name__ == '__main__':
+   st.header("ANALIZA SINALA")
+   amplitude = st.sidebar.number_input("Odaberi amplitudu [V]", 0.1, 230.0,1.)
+   sample_rate = st.sidebar.slider("Odaberi SAMPLE RATE [Hz]", 1, 44100,44100)    
+   st.sidebar.info("Frekvencija se resetira promjenom sampling-a (Nyquist frequiency)")
+   frequency = st.sidebar.number_input("Odaberi frekevenciju [Hz]", 1, int(sample_rate/2)) #nyquist frequency
+   time = st.sidebar.number_input("Odaberi trajanje u sekundama [s]", 1, 60)
+
+   pick_wave_gen = st.sidebar.radio(
+    "Odaberi val koji generirati",
+    [
+        "Sin","Cos","Sawtooth","Tirangle","Square","White noise","Brown noise"
+    ]
+    )
+   y = switch_waves(pick_wave_gen,amplitude,time,frequency,sample_rate)
+   Umax = max(y[:N_uzoraka])
+   Umin = min(y[:N_uzoraka])
+   Upp = Umax - Umin
+   Udc = dc_component(y)
+   Uef = efective(y)
+   standard_deviation = sp.ndimage.standard_deviation(y[:N_uzoraka])
+   gamma = float('inf') if (standard_deviation/Udc) > 100000.0 else (standard_deviation/Udc) * 100
+   Psr = Uef**2
+   Psr_dBW = 20 * np.log10(Uef) 
+   rounded_values = [round(x,16) for x in [Umax,Umin,Upp,Udc,Uef,standard_deviation,gamma,Psr,Psr_dBW ]]
+
+   table = pd.DataFrame( {
+                              "Values":rounded_values,
+                              #"":["V","V","V","V","V","%"]
+                              },
+                            index= ["Umax","Umin","Upp","Udc","Uef","σ","γ","Psr","Psr_dBW "])
+
+   start,end = st.slider('Podesi slider za ublizavanje na val (skalirano je po sampling * vrijeme)',0, sample_rate*time,(0,sample_rate*time))    
+
+   on = st.checkbox("Napravi analizu signala [uzima se do prvih 1,2M uzoraka]")
+
+   st.write(gen_plot(y[start:end],Umax,Umin,Udc,Uef,on))
+
+   # Ensure the values are in 16-bit range
+   audio = np.int16(y * 32767)
+   # Write to a MP3 file
+   sp.io.wavfile.write('wave.mp3', sample_rate, audio)
+   st.audio('wave.mp3')
+   st.dataframe(table,width=300)
+
+   
+
