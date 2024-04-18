2c3e6c1f0d5f4d01e7612a25da58ab2de0134e45
Suman <frano123suman@gmail.com>
2024-04-18 16:09:35 +0200

    main

--- sinus.py
+++ sinus.py
@@ -42,11 +42,10 @@ def faza_vala_plot(signal):
 def plot_frekvencijski_spekar(signal,sample_rate):
    magnituda = np.abs( np.fft.fft(signal) )
    frequency = np.linspace(0,sample_rate,len(magnituda))
-   frequency_bin = int(len(frequency*0.1))
    
    fig = plt.figure(figsize=(10,8))
    plt.subplot(211)
-   plt.plot(frequency[:frequency_bin],magnituda[:frequency_bin])
+   plt.plot(frequency,magnituda)
    plt.xlabel("Frequency (Hz)")
    plt.ylabel("Magnitude")
    plt.show()
