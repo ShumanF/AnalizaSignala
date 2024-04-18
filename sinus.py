93d236fa58c7d1a98f4c7a012c85c6290b488b88
Frano <frano123suman@gmail.com>
2024-04-19 01:34:30 +0200

    main

--- sinus.py
+++ sinus.py
@@ -133,7 +133,7 @@ if __name__ == '__main__':
   if pick_wave_gen == 'Uploaded File':
      time = int(len(signal)/44100)
 
-  t = len(signal)  
+  t = np.linspace(0, time, int(sample_rate * time), endpoint=False)   
   signal = np.exp(-t*Prigušenje) * signal
   start,end = st.slider('Podesi slider za ublizavanje na val (skalirano je po sampling * vrijeme, slider ide od 0 do N samples)',0, sample_rate*time,(0,sample_rate))    
 
